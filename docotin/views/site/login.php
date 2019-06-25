ff0bc98db1d152bc3c32617680d22b937d6858db
miftahhafifudin <miftahhafifudin0811@gmail.com>
2019-06-25 15:55:16 +0700

    login & register

--- docotin/views/site/login.php
+++ docotin/views/site/login.php
@@ -10,38 +10,73 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+<html lang="en"><head>
+    <?php
+            $form = ActiveForm::begin() ?>
+  </head>
+  <body>
+    
 
-    <p>Please fill out the following fields to login:</p>
+    <!-- <input type="hidden" id="menu" value="flagging"> -->
+<!--    <nav class="navbar navbar-dark">
+            <div class="container-fluid"> -->
+              <!-- Brand and toggle get grouped for better mobile display -->
+            <!--   <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Docotel</a>
+                <a class="navbar-brand" href="#">Login</a>
+                <a class="navbar-brand" href="#">Register</a>
+              </div> -->
 
-    <?php $form = ActiveForm::begin([
-        'id' => 'login-form',
-        'layout' => 'horizontal',
-        'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-1 control-label'],
-        ],
-    ]); ?>
-
-        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-        <?= $form->field($model, 'password')->passwordInput() ?>
-
-        <?= $form->field($model, 'rememberMe')->checkbox([
-            'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-        ]) ?>
-
-        <div class="form-group">
-            <div class="col-lg-offset-1 col-lg-11">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+    <!-- </nav> -->
+    
+        <div class="login-content">
+            <div class="panel">
+                <div class="panel-heading">LOGIN</div>
+                <div class="panel-body">
+                    <form class="form-horizontal">
+                        <div class="login-form">
+                            <table class="custom-table">
+                                <tbody>
+                                    <tr class="success">
+                                        <td><h5> <?= $form->field($model, 'username')->textInput() ?>
+                                                    <?php $option = ['class'=>'form-control'];
+                                                    echo Html::addCssStyle($option,'form-control');
+                                                    ?>
+                                        </h5></td>
+                                    </tr>
+                                    <tr class="success">
+                                        <td><h5> <?= $form->field($model, 'password')->passwordInput() ?>
+                                                <?php $option = ['class'=>'form-control'];
+                                                 echo Html::addCssStyle($option,'form-cotrol');
+                                                 ?>
+                                        </h5></td>
+                                    </tr>
+                                    <tr>
+                                        <td type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                                        <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                                        </td>
+                                        <td type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                                        <?=Html::a('Register',['/user/register'],['class'=>'btn btn-primary'])
+                                        ?>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td><a href="">Forget Password ?</a></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-
-    <?php ActiveForm::end(); ?>
-
-    <div class="col-lg-offset-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
-    </div>
-</div>
+</body>
+<?php ActiveForm::end() ?>
+</html>
--- docotin/views/user/register.php
+++ docotin/views/user/register.php
@@ -8,62 +8,173 @@ use app\models\Grup;
 
 $this->title = 'Register';
 ?>
-<div class="site-index">
-	<h1>Register</h1>
 
-	<div class="body-content">
-		<?php
-		$form = ActiveForm::begin()?>
+<html lang="en"><head>
+	<?php
+			$form = ActiveForm::begin() ?>
+			
+  </head>
+  <body>
+	
 
-		<div class="row">
-			<div class="form-group">
-				<div class="col-lg-6">
-				<?= $form->field($model,'username');?>
+	<!-- <input type="hidden" id="menu" value="flagging">
+	<nav class="navbar navbar-dark">
+			<div class="container-fluid">
+			   Brand and toggle get grouped for better mobile display -->
+			<!--   <div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				  <span class="sr-only">Toggle navigation</span>
+				  <span class="icon-bar"></span>
+				  <span class="icon-bar"></span>
+				  <span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="">Docotel</a>
+				<a class="navbar-brand" href="../site/login.php">Login</a>
+				<a class="navbar-brand" href="#">Register</a>
+			  </div> -->
+
+<!-- 	</nav> -->
+	 -->
+
+	<div class="login-content">
+			<div class="panel">
+				<div class="panel-heading">REGISTER</div>
+				<div class="panel-body">
+					<form class="form-horizontal">
+						<div class="login-form">
+							<table class="custom-table">
+								<tbody>
+									<tr class="success">
+										<div class="radio">
+											<?php $cat=Grup::find()->all();
+											$listData=ArrayHelper::map($cat,'id_grup','nama_grup');
+											echo $form->field($model, 'id_grup')->radioList(
+									        $listData,
+									        ['prompt'=>'Select'],
+									        ['width'=>'200px']
+									        );?>
+										</div>
+									</tr>
+								</tbody>
+							</table>
+							<div class="form-pembeli">
+								<table>
+
+									<tr>
+										<td><h5> 
+										<div class="register"><?= $form->field($model,'username');?>
+										</div></h5></td>
+									</tr>
+
+									<tr>
+										<td><h5> 
+										<div class="register">
+										<?= $form->field($model,'email'); ?>
+										</div></h5></td>
+									</tr>
+									<tr>
+										<td><h5>
+										 <div class="register">
+										 	<?= $form->field($model,'password')->passwordInput();?>	
+										 </div></h5></td>
+										
+									</tr>
+									<!-- <tr>
+										<td><h5> <div class="register">Repassword</div></h5></td>
+										<td><input type="password" class="form-control" id="inputEmail3"></td>
+									</tr> -->
+									<tr>
+										<td><h5> <div class="register"><?= $form->field($model,'lantai');?>	
+										</div></h5></td>
+									</tr>
+									<tr>
+										<td><h5> <div class="register"><?= $form->field($model,'no_hp');?>
+										</div></h5></td>
+									</tr>
+									<tr>
+										<td></td>
+										<td>
+											<!-- <button class="" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> -->
+					<?= Html::submitButton('REGISTER',['class'=>'btn btn-primary','type'=>'button','data-toggle'=>'collapse','data-target'=>'#collapseExample','aria-expanded'=>'false','aria-controls'=>'collapseExample"']);?>
+														
+												<!-- </button> -->
+										</td>
+									</tr>
+									<tr>
+										<td></td>
+									</tr>
+								</table>
+							</div>
+							<div class="form-penjual">
+								<table>
+									<tr>
+										<td><h5> <div class="register">Username</div></h5></td> 
+										<td><input type="text" class="form-control" id="inputEmail3"></td>
+									</tr>
+									<tr>
+										<td><h5> <div class="register">Email Address</div></h5></td>
+										<td><input type="text" class="form-control" id="inputEmail3"></td>
+									</tr>
+									<tr>
+										<td><h5> <div class="register">Password</div></h5></td>
+										<td><input type="password" class="form-control" id="inputEmail3"></td>
+									</tr>
+									<tr>
+											<td><h5> <div class="register">Repassword</div></h5></td>
+										<td><input type="password" class="form-control" id="inputEmail3"></td>
+									</tr>
+									<tr>
+										<td><h5> <div class="register">NPWP Number</div></h5></td>
+										<td><input type="number" class="form-control" id="inputEmail3"></td>
+									</tr>
+									<tr>
+										<td><h5> <div class="register">No Handphone</div></h5></td>
+										<td><input type="number" class="form-control" id="inputEmail3"></td>
+									</tr>
+									<tr>
+										<td></td>
+										<td><!-- 
+											<button class="btn btn-primary " type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> -->
+												<?= Html::submitButton('REGISTER',['class'=>'btn btn-primary']);?>
+											</button>
+										</td>
+									</tr>
+									<tr>
+										<td></td>
+									</tr>
+								</table>
+							</div>
+						</div>
+					</form>
 				</div>
 			</div>
+			
 		</div>
+	
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<script src="../scripts/main.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('.form-penjual').hide()					
 
-		<div class="row">
-			<div class="form-group">
-				<div class="col-lg-6">
-				<?= $form->field($model,'password')->passwordInput();?>
-				</div>
-			</div>
-		</div>
-
-		<div class="row">
-			<div class="form-group">
-				<div class="col-lg-6">
-				<?= $form->field($model,'email');?>
-				</div>
-			</div>
-		</div>
-
-		 <div class="row">
-            <div class="form-group">
-                <div class="col-lg-6">
-		       <?php $cat=Grup::find()->all();
-				$listData=ArrayHelper::map($cat,'id_grup','nama_grup');
-				echo $form->field($model, 'id_grup')->dropDownList(
-		        $listData,
-		        ['prompt'=>'Select']
-		        );?>
-             	</div>
-            </div>
-        </div>
-
-		<div class="row">
-        	<div class="form-group">
-        		<div class="col-lg-6">
-        			<div class="col-lg-3">
-        			<?= Html::submitButton('REGISTER',['class'=>'btn btn-primary']);?>
-        			</div>
-        			<div class="col-lg-2">
-        			<a href=<?php echo Yii::$app->homeUrl; ?> class=btn btn-primary >go back</a>
-        			</div>
-        		</div>
-        	</div>
-    	</div>
-	<?php ActiveForm::end() ?>
-</div>
-</div>
+	 		$('input[name=optradio]').on('change', function() {
+				values=$('input[name=optradio]:checked').val()
+				if(values == "pembeli"){
+					$('.form-pembeli').show()
+					$('.form-penjual').hide()					
+				} else {
+					$('.form-pembeli').hide()
+					$('.form-penjual').show()
+				}
+			});
+				
+		});
+	</script>
+</body>
+<?php ActiveForm::end() ?>
+</html>
