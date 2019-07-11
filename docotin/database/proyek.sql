85b3b2305f6a8a5f0b70c95c49b9f62ec4a4e7a4
floridia <floridia@docotel.co.id>
2019-07-11 11:09:10 +0700

    Commit Tabel Tambahan

--- docotin/database/proyek.sql
+++ docotin/database/proyek.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 19, 2019 at 06:53 AM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.10
+-- Generation Time: Jul 11, 2019 at 06:07 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `proyek`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `harga_barang` int(20) NOT NULL,
+  `gambar_barang` text NOT NULL,
+  `stok` int(10) NOT NULL,
+  `deksripsi` text NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `nama_barang`, `harga_barang`, `gambar_barang`, `stok`, `deksripsi`, `id_user`) VALUES
+(4, 'Mie', 10000, '/uploads/barang/mie.jpg', 1, 'Barang Bagus nih', 31),
+(5, 'Kebab', 10000, '/uploads/barang/Resep-Kebab.jpg', 1, 'Kebab Hangat dapat di Simpan di Microwave', 31);
 
 -- --------------------------------------------------------
 
@@ -56,6 +80,20 @@ CREATE TABLE `saldo` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `status_bayar` varchar(50) NOT NULL,
+  `tanggal_bayar` date NOT NULL,
+  `tanggal_transaksi` date NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -85,6 +123,13 @@ INSERT INTO `user` (`id_user`, `id_grup`, `username`, `password`, `email`) VALUE
 --
 
 --
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`),
+  ADD KEY `id_user` (`id_user`);
+
+--
 -- Indexes for table `grup`
 --
 ALTER TABLE `grup`
@@ -97,6 +142,12 @@ ALTER TABLE `saldo`
   ADD PRIMARY KEY (`id_saldo`);
 
 --
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -107,22 +158,39 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
 -- AUTO_INCREMENT for table `grup`
 --
 ALTER TABLE `grup`
   MODIFY `id_grup` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
 --
 -- AUTO_INCREMENT for table `saldo`
 --
 ALTER TABLE `saldo`
   MODIFY `id_saldo` int(11) NOT NULL AUTO_INCREMENT;
-
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `barang`
+--
+ALTER TABLE `barang`
+  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
