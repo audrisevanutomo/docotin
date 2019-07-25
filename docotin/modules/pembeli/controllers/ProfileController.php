ca9df765da2f0a540589382d8114bcd2a193038f
ratihdlestari <ratih.lestari18@gmail.com>
2019-07-25 13:18:44 +0700

    fungsi update profile pembeli

--- docotin/modules/pembeli/controllers/ProfileController.php
+++ docotin/modules/pembeli/controllers/ProfileController.php
@@ -2,7 +2,15 @@
 
 namespace app\modules\pembeli\controllers;
 
+use Yii;
 use yii\web\Controller;
+use app\models\User;
+use app\models\Grup;
+use app\models\BackedUser;
+use app\models\DetailUser;
+use app\models\DetailUserForm;
+use yii\data\ActiveDataProvider;
+
 
 /**
  * Default controller for the `pembeli` module
@@ -16,8 +24,30 @@ class ProfileController extends Controller
      */
     public function actionProfilePembeli()
     {
-        return $this->render('profilepembeli');
-    }
+        $id = Yii::$app->user->id;
+        $user = BackedUser::findOne($id); 
+        $detailUserAwal = DetailUser::find()
+                        ->where(['id_user' => $id])
+                        ->one();
+        $model = new DetailUserForm();
+        if($detailUserAwal !== null){
+            $model->attributes=$detailUserAwal->attributes;
+        }
+
+        if($model->load(Yii::$app->request->post()) && $model->validate()) {
+            $detailUser = new DetailUser();
+            $detailUser->id_user = $id;
+            if ($detailUser->saveModel($model, $detailUserAwal)) {
+                Yii::$app->session->setFlash('success', "Biodata berhasil diubah!");
+                return $this->redirect(['profile-pembeli', 'id' => $detailUser->id ]);
+        }
+        //var_dump($user); exit;
+        //$pengguna = BackedUser::findOne($user->id_user);
+        
+        }
+        return $this->render('profilepembeli', ['user'=>$user, 'model'=>$model]);
+    }   
+
     public function actionListTopup()
     {
         return $this->render('listtopup');
@@ -26,4 +56,5 @@ class ProfileController extends Controller
     {
         return $this->render('detailtopup'); 
     }
+    
 }
