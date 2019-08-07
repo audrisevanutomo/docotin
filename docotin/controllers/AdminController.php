ca1bd41cd51546b1f00c6573999386fd061b1dbb
miftahhafifudin <miftahhafifudin0811@gmail.com>
2019-08-07 10:30:08 +0700

    pembaruan halaman list

--- docotin/controllers/AdminController.php
+++ docotin/controllers/AdminController.php
@@ -3,12 +3,14 @@
 namespace app\controllers;
 
 use yii;
-	use yii\web\Controller;
-	use app\models\LoginForm;
-	use app\models\BackedUser;
-	use app\models\Transaksi;
-    use app\models\TransaksiSearch;
+    use yii\web\Controller;
+    use app\models\LoginForm;
+    use app\models\BackedUser;
+    use app\models\Transaksi;
+    use app\models\Search;
     use yii\data\Pagination;
+    use yii\data\Sort;
+    use yii\db\ActiveQuery;
 
 class AdminController extends \yii\web\Controller
 {
@@ -17,28 +19,77 @@ class AdminController extends \yii\web\Controller
         return $this->render('index');
     }
 
-    	public function actionList(){
-    		$list = Transaksi::find();
+        public function actionList(){
+
+            if(Yii::$app->user->isGuest){
+                return $this->redirect(['site/login']);
+            }
+            if(Yii::$app->user->identity->id_grup==2){
+                return $this->redirect(['site/index']);
+            }
+
+            if(Yii::$app->user->identity->id_grup==1){
+                return $this->redirect(['site/index']);
+            }
+
+            $sort = new sort([
+                'attributes'=>[
+                    'status_topup'=>[
+                        'asc'=>['status_topup'=>SORT_ASC],
+                        'desc'=>['status_topup'=>SORT_DESC],
+                        'default'=>SORT_DESC,
+                        'label' => 'status'
+                        ],
+                    ],
+                ]);
+            $list = Search::find();
+            $post = Yii::$app->request->queryParams;
+            $conditions =[];
+            if(!empty($post['Search']['status_topup'])){
+                $conditions['status_topup']=$post['Search']['status_topup'];
+            }
+             if(!empty($post['Search']['id_topup'])){
+                $conditions['id_topup']=$post['Search']['id_topup'];
+            }
+                // var_dump($post['Search']);exit;
+            if(!empty($post['Search']['nama_lengkap'])){
+                // $conditions['BackedUser.nama_lengkap']=$post['Search']['nama_lengkap'];
+                $list = $list->joinWith('backedUser bu');
+                $conditions['bu.nama_lengkap']=$post['Search']['nama_lengkap'];
+                   
+            }
+                // var_dump(Yii::$app->request);exit;
+                $list= $list ->where($conditions);
             //unfinished pagination. fix it or leave it
+            // var_dump($list);exit;
             $pages = new Pagination([
-                    'defaultPageSize'=>7,
-                    'totalCount'=>count($list),
+                    'defaultPageSize'=>5,
+                    'totalCount'=>count($list->all()),
                     ]);
             $models = $list->orderBy('id_topup')
                 ->offset($pages->offset)
                 ->limit($pages->limit)
                 ->all();
-            if(Yii::$app->user->identity->id_grup==2){
-                return $this->redirect(['site/index']);
-                }
+
+            $searchModel = new Search();
+            //$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
             // var_dump($list[0]->backedUser);exit;
-        	// $user = $list->backedUser;
-    		return $this->render('list',[
+            // $user = $list->backedUser;
+            return $this->render('list',[
+                'searchModel'=>$searchModel,
+                'sort'=>$sort,
                 'models'=>$models,
                 'pages'=>$pages,
                 'list'=>$list,
             ]);
-    	} 
+        } 
+
+        public function actionSearch(){
+            $searchModel = Search::find()
+                    ->where(['id_topup'=>Yii::$app->request->queryParams]);
+            return $this->render('list',['searchModel'=>$searchModel]);
+            //$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+        }
 
         public function actionDetail($id){
             $detail = BackedUser::findOne($id);
--- docotin/views/admin/_search.php
+++ docotin/views/admin/_search.php
@@ -1,20 +1,25 @@
 <?php
 	use yii\helpers\Html;
 	use yii\widgets\ActiveForm;
+    // var_dump($model);exit;
 ?>
 
 <div class="data-search">
 
-	<?php $form = ActiveForm([
-		'action'=>['list'],
-		'method'=>'get',
-	]);?>
+	  <?php $form = ActiveForm::begin([
+        'action' => ['list'],
+        'method' => 'get',
+    ]); ?>
+<div class="row">
+    
+	<?= $form->field($model,'id_topup',['options'=>['class'=>'col col-lg-4']]) ?> 
+    <?= $form->field($model,'status_topup',['options'=>['class'=>'col col-lg-4']]) ?>
+    <?= $form->field($model,'nama_lengkap',['options'=>['class'=>'col col-lg-4']]) ?>
 
-	<?= $form->field($model,'id_topup') ?>
+</div>
 
 	<div class="form-group">
         <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
-        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
     </div>
   <?php ActiveForm::end(); ?>
 
